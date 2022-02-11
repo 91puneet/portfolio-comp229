80d7fe9ab923c6fe5a8013c92ccb87bbf9308d33
91puneet <91puneetsingh@gmail.com>
2022-02-11 16:45:16 -0500

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('homepage', { title: 'HOME' });
+});
+
+/* GET home page. */
+router.get('/home', function(req, res, next) {
+  res.render('homepage', { title: 'HOME' });
+});
+
+/* GET About me page. */
+router.get('/about', function(req, res, next) {
+  res.render('aboutmepage', { title: 'About' });
+});
+/* GET Projects page. */
+router.get('/projects', function(req, res, next) {
+  res.render('projectspage', { title: 'Projects' });
+});
+/* GET Services page. */
+router.get('/services', function(req, res, next) {
+  res.render('servicespage', { title: 'Services' });
+});
+/* GET  Contact Me page. */
+router.get('/contact', function(req, res, next) {
+  res.render('contactmepage', { title: 'contact' });
+});
+ 
+
+module.exports = router;
