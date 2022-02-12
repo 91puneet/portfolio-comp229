815a657274d8c7d64f5e7273e2094274372967f5
91puneet <91puneetsingh@gmail.com>
2022-02-11 21:17:58 -0500

    final commit

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('homepage', { title: 'HOME' });
+  res.render('home', { title: 'HOME' });
 });
 
 /* GET home page. */
